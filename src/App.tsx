9589304a149ef5f418650181fc364d083342d1f3
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-05-09 08:43:04 -0400

    fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<Signup/>} />
+      <Route path='' element='' />
     </Routes>
   )
 }
