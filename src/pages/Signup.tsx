94150ea1a82f482b56246b9082d122a36bed8bf8
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-05-09 08:59:28 -0400

    fixed signup component

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,16 +2,14 @@ const Signup = () => {
   return (
     <form>
       <div className='hero min-h-screen bg-base-200'>
-        <div className='hero-content flex-col lg:flex-row-reverse'>
-          <div className='text-center lg:text-left'>
-            <h1 className='text-5xl font-bold'>Login now!</h1>
+        <div className='hero-content flex-col '>
+          <div className='text-center '>
+            <h1 className='text-5xl font-bold'>Image Gallery Pro</h1>
             <p className='py-6'>
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Sign up today and start sharing your photos with the world!
             </p>
           </div>
-          <div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+          <div className='card sm:w-[30rem] 9shadow-2xl bg-base-100'>
             <form className='card-body'>
               <div className='form-control'>
                 <label className='label'>
@@ -34,14 +32,10 @@ const Signup = () => {
                   className='input input-bordered'
                   required
                 />
-                <label className='label'>
-                  <a href='#' className='label-text-alt link link-hover'>
-                    Forgot password?
-                  </a>
-                </label>
+
               </div>
               <div className='form-control mt-6'>
-                <button className='btn btn-primary'>Login</button>
+                <button className='btn btn-primary'>Singup</button>
               </div>
             </form>
           </div>
