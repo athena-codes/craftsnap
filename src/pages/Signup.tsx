9a8e40c9a7fa5ec79590a39eb98545458543d1be
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-05-13 14:26:54 -0400

    signup working, firebase connected :D

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -18,58 +18,60 @@ const Signup = () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password)
       navigate('/')
-
-    } catch (error: Error) {
+    } catch (error) {
       setError(error.message)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='hero min-h-screen bg-base-200'>
-        <div className='hero-content flex-col '>
-          <div className='text-center '>
-            <h1 className='text-5xl font-bold'>Image Gallery Pro</h1>
-            <p className='py-6'>
-              Sign up today and start sharing your photos with the world!
-            </p>
-          </div>
-          <div className='card sm:w-[30rem] 9shadow-2xl bg-base-100'>
-            <form className='card-body'>
-              <div className='form-control'>
-                <label className='label'>
-                  <span className='label-text'>Email</span>
-                </label>
-                <input
-                  type='email'
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                  placeholder='email'
-                  className='input input-bordered'
-                  required
-                />
+    <>
+      <form onSubmit={handleSubmit}>
+        {error && error}
+        <div className='hero min-h-screen bg-base-200'>
+          <div className='hero-content flex-col '>
+            <div className='text-center '>
+              <h1 className='text-5xl font-bold'>Image Gallery Pro</h1>
+              <p className='py-6'>
+                Sign up today and start sharing your photos with the world!
+              </p>
+            </div>
+            <div className='card sm:w-[30rem] 9shadow-2xl bg-base-100'>
+              <div className='card-body'>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Email</span>
+                  </label>
+                  <input
+                    type='email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder='email'
+                    className='input input-bordered'
+                    required
+                  />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Password</span>
+                  </label>
+                  <input
+                    type='password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder='password'
+                    className='input input-bordered'
+                    required
+                  />
+                </div>
+                <div className='form-control mt-6'>
+                  <button className='btn btn-primary'>Sign Up</button>
+                </div>
               </div>
-              <div className='form-control'>
-                <label className='label'>
-                  <span className='label-text'>Password</span>
-                </label>
-                <input
-                  type='password'
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  placeholder='password'
-                  className='input input-bordered'
-                  required
-                />
-              </div>
-              <div className='form-control mt-6'>
-                <button className='btn btn-primary'>Sign Up</button>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   )
 }
 
