2eecb9300518cfba0c2f233428f1f65f54303d05
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-05-13 14:42:24 -0400

    logout working c:

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase/config"
+
 const Navbar = () => {
+   const handleLogout = async () => {
+    try {
+      await signOut(auth)
+    } catch (error) {
+      console.log(error.message)
+    }
+   }
+
   return (
     <div className='navbar bg-base-100 justify-between'>
       <a className='font-bold normal-case underline text-xl'>
         Image Gallery Pro <span>📸</span>
       </a>
-      <button>Log Out</button>
+      <button onClick={handleLogout}>Log Out</button>
     </div>
   )
 }
