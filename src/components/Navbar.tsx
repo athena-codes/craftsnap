a101f45fc742c57d320f8a4c1f8ad41fb940d70e
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-05-15 15:22:28 -0400

    added icon to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,28 @@
 import { signOut } from "firebase/auth"
 import { auth } from "../firebase/config"
+import  minecraftIcon from '../assets/4737386_minecraft_squircle_icon.png'
 
 const Navbar = () => {
-   const handleLogout = async () => {
+  const handleLogout = async () => {
     try {
       await signOut(auth)
     } catch (error) {
       console.log(error.message)
     }
-   }
+  }
 
   return (
-    <div className='navbar bg-base-100 justify-between'>
-      <a className='font-bold normal-case text-xl'>
-        Craft Snap<span>⛏️💎</span>
-      </a>
+    <div className='navbar bg-base-100 justify-between items-center'>
+      <div className='flex items-center'>
+        <img
+          src={minecraftIcon}
+          alt='CraftSnap Logo'
+          style={{ width: '40px', height: '40px', marginRight: '10px' }}
+        />
+        <a className='font-bold normal-case text-xl'>
+          CraftSnap<span></span>
+        </a>
+      </div>
       <button onClick={handleLogout}>Log Out</button>
     </div>
   )
